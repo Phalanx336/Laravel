88d3824921febfe90e5030fab57dd624fc856839
Phalanx336 <kmharvey@coastal.edu>
2020-02-23 21:20:33 -0500

    Please enter the commit message for your changes. Lines starting

---
+++ csci409/database/migrations/2020_02_23_230046_add_adress_to_hotels.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAdressToHotels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::update('hotels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('address_1');
+            $table->string('address_2');
+            $table->string('city')->nullable();
+            $table->string('state');
+            $table->string('zip');
+            $table->string('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotels');
+    }
+}
