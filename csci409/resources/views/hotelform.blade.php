47be4044901ca1ecccf39c2dfa876a2f5c5de57d
Phalanx336 <kmharvey@coastal.edu>
2020-04-03 19:20:13 -0400

    Added create hotel form

---
+++ csci409/resources/views/hotelform.blade.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<form method="post" action="/hotels/store">
+    <label for="name">Name:</label><br>
+    <input type="text" id="name" value="John">
+    <label for="address 1">Name:</label><br>
+    <input type="text" id="address 1" value="Address">
+    <label for="address 2">Name:</label><br>
+    <input type="text" id="address 2" value="Address">
+    <label for="city">Name:</label><br>
+    <input type="text" id="city" value="City">
+    <label for="state">Name:</label><br>
+    <input type="text" id="state" value="State">
+    <label for="zip">Name:</label><br>
+    <input type="text" id="zip" value="Zip">
+    <label for="description">Name:</label><br>
+    <input type="text" id="description" value="Description">
+    <input type="submit" value="submit">
+</form>
+
+<?php
+
